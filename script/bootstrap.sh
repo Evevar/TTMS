fee289aa63d257b7f9b267bb2a778b0ddb9b5aab
123kangning <3390142252@qq.com>
2023-05-27 17:43:06 +0800

    分离编译和运行的脚本

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -16,15 +16,9 @@ echo "" >${STUDIO_LOG_FILE}
 echo "" >${ORDER_LOG_FILE}
 
 # 启动程序，并将输出重定向到日志文件
-go build -o ttms_web ../internal/web
 ./ttms_web >>${WEB_LOG_FILE} 2>&1 &
-go build -o ttms_user ../internal/user
 ./ttms_user >>${USER_LOG_FILE} 2>&1 &
-go build -o ttms_play ../internal/play
 ./ttms_play >>${PLAY_LOG_FILE} 2>&1 &
-go build -o ttms_ticket ../internal/ticket
 ./ttms_ticket >>${TICKET_LOG_FILE} 2>&1 &
-go build -o ttms_studio ../internal/studio
 ./ttms_studio >>${STUDIO_LOG_FILE} 2>&1 &
-go build -o ttms_order ../internal/order
 ./ttms_order >>${ORDER_LOG_FILE} 2>&1 &
---
+++ script/build.sh
@@ -0,0 +1,6 @@
+go build -o ttms_web ../internal/web
+go build -o ttms_user ../internal/user
+go build -o ttms_play ../internal/play
+go build -o ttms_ticket ../internal/ticket
+go build -o ttms_studio ../internal/studio
+go build -o ttms_order ../internal/order
